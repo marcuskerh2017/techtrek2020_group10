f7076cff60c9026adc2da27efdeaa474b92a8a3c
Athena Chan <e0191456@u.nus.edu>
2020-09-25 11:43:56 +0800

    home page button link to form page

--- customer_service_app/src/views/HomePage.jsx
+++ customer_service_app/src/views/HomePage.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable react/state-in-constructor */
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { withRouter } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core/styles";
+
 const styles = (theme) => ({
   root: {
     height: "100vh",
@@ -19,6 +20,20 @@ class HomePage extends Component {
         <Typography component="div">
           <h1 className="header">Home Page</h1>
         </Typography>
+        <section class="text-gray-700 body-font">
+          <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+            {/* <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="https://images.unsplash.com/flagged/photo-1562503542-2a1e6f03b16b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1936&q=80" /> */}
+            <img alt="gallery" class="w-full object-cover h-full object-center block opacity-25 absolute inset-0" src="https://images.unsplash.com/flagged/photo-1562503542-2a1e6f03b16b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1936&q=80" />
+            <div class="text-center lg:w-2/3 w-full">
+              <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Microdosing synth tattooed vexillologist</h1>
+              <div class="flex justify-center">
+                <Link to="/form">
+                  <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Register A Customer</button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </section>
         <div className="developed-text">Built by TeckTrek</div>
       </>
     );
