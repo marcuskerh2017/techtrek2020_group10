a39eeb6d4dfc6efc20521ec4766a969bc63871ed
yanbutan <ynaung6995@gmail.com>
2020-09-25 14:41:57 +0800

    timer session

--- customer_service_app/src/components/Header.js
+++ customer_service_app/src/components/Header.js
@@ -1,47 +1,124 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Countdown from "react-countdown";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 
 export default function Header() {
-  return (
-    <header class="text-gray-700 body-font">
-      <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-between">
+  let url =
+    "http://techtrek2020.ap-southeast-1.elasticbeanstalk.com/extendSession";
+  let token = window.localStorage.getItem("token");
+  let countDownTime = window.localStorage.getItem("expDate");
+  const [expTime, setExpTime] = useState(null);
+  const [open, setOpen] = React.useState(false);
+  const [count, setCount] = useState(0);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-        <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-          <img class="w-25 h-20 text-white p-2" src="https://logos-download.com/wp-content/uploads/2016/12/DBS_Bank_logo_logotype.png" />
-          {/* <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-          </svg>
-          <span class="ml-3 text-xl">Tiim 10</span> */}
-        </a>
-        {/* <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-          <a class="mr-5 hover:text-gray-900">First Link</a>
-          <a class="mr-5 hover:text-gray-900">Second Link</a>
-          <a class="mr-5 hover:text-gray-900">Third Link</a>
-          <a class="mr-5 hover:text-gray-900">Fourth Link</a>
-        </nav> */}
-        {/* <button class="inline-flex items-center bg-gray-200 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base mt-4 md:mt-0">
-          Login
-          <svg
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            class="w-4 h-4 ml-1"
-            viewBox="0 0 24 24"
-          >
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
-        </button> */}
-      </div>
-    </header>
-  );
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    let countDownTime = window.localStorage.getItem("expDate");
+    if (countDownTime != null) {
+      setExpTime(new Date(countDownTime));
+    }
+  }, [count]);
+
+  const handleExtend = (e) => {
+    e.preventDefault();
+    console.log("extending session");
+    fetch(url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => {
+        return res.text();
+      })
+      .then((data) => {
+        window.localStorage.setItem("token", data);
+        window.localStorage.setItem(
+          "expDate",
+          new Date(new Date().getTime() + 1 * 60 * 1000)
+        );
+        setCount(count + 1);
+        handleClose();
+      })
+      .catch((err) => console.log(err));
+  };
+  // const [timeLeft, setTimeLeft] = useState(false);
+  // if (countDownTime != null) {
+  //   let myfunc = setInterval(function () {
+  //     let now = new Date().getTime();
+  //     let timeleft = new Date(countDownTime) - now;
+
+  //     let days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+  //     let hours = Math.floor(
+  //       (timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  //     );
+  //     let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+  //     let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+  //     if (timeLeft < 0) {
+  //       alert("Hello time to renew");
+  //     }
+  //   }, 1000);
+  // }
+
+  // console.log(new Date(countDownTime).getTime());
+  if (expTime) {
+    return (
+      <header class="text-gray-700 body-font">
+        <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-between">
+          <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <img
+              class="w-25 h-20 text-white p-2"
+              src="https://logos-download.com/wp-content/uploads/2016/12/DBS_Bank_logo_logotype.png"
+            />
+          </a>
+          <Countdown date={expTime} onComplete={handleClickOpen} />
+        </div>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            Bro you want extend session?
+          </DialogTitle>
+          {/* <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Bro you want extend session?
+            </DialogContentText>
+          </DialogContent> */}
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Don't want
+            </Button>
+            <Button onClick={handleExtend} color="primary" autoFocus>
+              Extend
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </header>
+    );
+  } else
+    return (
+      <header class="text-gray-700 body-font">
+        <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-between">
+          <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <img
+              class="w-25 h-20 text-white p-2"
+              src="https://logos-download.com/wp-content/uploads/2016/12/DBS_Bank_logo_logotype.png"
+            />
+          </a>
+        </div>
+      </header>
+    );
 }
