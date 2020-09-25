004031a3d071c895fddef03d57dbe5ed539a6911
Athena Chan <e0191456@u.nus.edu>
2020-09-25 12:30:49 +0800

    changed header logo

--- customer_service_app/src/components/Header.js
+++ customer_service_app/src/components/Header.js
@@ -4,8 +4,10 @@ export default function Header() {
   return (
     <header class="text-gray-700 body-font">
       <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-between">
+
         <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-          <svg
+          <img class="w-25 h-20 text-white p-2" src="https://logos-download.com/wp-content/uploads/2016/12/DBS_Bank_logo_logotype.png" />
+          {/* <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             stroke="currentColor"
@@ -17,7 +19,7 @@ export default function Header() {
           >
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
           </svg>
-          <span class="ml-3 text-xl">Tiim 10</span>
+          <span class="ml-3 text-xl">Tiim 10</span> */}
         </a>
         {/* <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
           <a class="mr-5 hover:text-gray-900">First Link</a>
@@ -25,7 +27,7 @@ export default function Header() {
           <a class="mr-5 hover:text-gray-900">Third Link</a>
           <a class="mr-5 hover:text-gray-900">Fourth Link</a>
         </nav> */}
-        <button class="inline-flex items-center bg-gray-200 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base mt-4 md:mt-0">
+        {/* <button class="inline-flex items-center bg-gray-200 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base mt-4 md:mt-0">
           Login
           <svg
             fill="none"
@@ -38,7 +40,7 @@ export default function Header() {
           >
             <path d="M5 12h14M12 5l7 7-7 7"></path>
           </svg>
-        </button>
+        </button> */}
       </div>
     </header>
   );
